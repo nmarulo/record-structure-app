c08640b096bc50d0c30b5808c6bf0fa24cb01d65
Nicolás Marulanda P <nmarulo@outlook.com>
2025-06-02 22:05:53 +0200

    Agregado: FormControl para el input de selección de archivos

--- src/app/modules/home/pages/home/home.ts
+++ src/app/modules/home/pages/home/home.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+
+  fileInput: FormControl = new FormControl('', {nonNullable: true});
+
+  constructor() {
+  }
 
 }
