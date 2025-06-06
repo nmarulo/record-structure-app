4b98559084887a5442960b2268c8c91c4c9b8c3b
Nicolás Marulanda P <nmarulo@outlook.com>
2025-06-07 01:32:03 +0200

    Agregado: Servicio "ElectronService" con método para seleccionar archivos en entorno Electron

---
+++ src/app/services/electron.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {from, Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ElectronService {
+
+  private readonly electronAPI: any;
+
+  constructor() {
+    this.electronAPI = (window as any).electronAPI;
+  }
+
+  get isElectron(): boolean {
+    return !!(window && this.electronAPI);
+  }
+
+  selectFile() {
+    if (this.isElectron) {
+      return from(this.electronAPI.selectFile()) as Observable<string>;
+    }
+
+    return from('');
+  }
+}
