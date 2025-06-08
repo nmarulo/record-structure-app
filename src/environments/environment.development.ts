b0722f34fa67dfa46c9d38c2864f899d8cbac270
Nicolás Marulanda P <nmarulo@outlook.com>
2025-06-08 17:05:56 +0200

    Agregado: Configuración para entornos en Angular

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  RECORD_STRUCTURE_API_URL: 'http://localhost:8080'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  RECORD_STRUCTURE_API_URL: 'http://localhost:8099'
+};
