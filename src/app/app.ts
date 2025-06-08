edf66ffae098ea311ffa648ae9bb9e6738bd67de
Nicolás Marulanda P <nmarulo@outlook.com>
2025-06-08 02:06:41 +0200

    Agregado: Pantalla de carga mientras se conecta al backend y verificación del estado del backend

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {ActuatorService} from '@app/services/pages/actuator.service';
+import {ElectronService} from '@app/services/electron.service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,41 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected title = 'record-structure-app';
+
+  backendReady = signal(false);
+
+  constructor(
+    private electronService: ElectronService,
+    private actuatorService: ActuatorService) {
+  }
+
+  ngOnInit() {
+    this.electronService.startSpringBoot()
+        .subscribe();
+    this.checkBackendStatus();
+  }
+
+  private checkBackendStatus() {
+    const checkInterval = setInterval(() => {
+      this.actuatorService.health()
+          .subscribe({
+            next: (response) => {
+              this.backendReady.set(response.status == 'UP');
+
+              if (this.backendReady()) {
+                clearInterval(checkInterval);
+                console.log('Backend conectado correctamente');
+              } else {
+                console.log('Backend no disponible');
+              }
+            },
+            error: () => {
+              console.log('Backend no disponible, reintentando...');
+            }
+          });
+    }, 1000);
+  }
+
 }
